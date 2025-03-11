b236ebbbb5d7589eac08fc222a42bbb8edabd171
spaceybread <spaceybread@gmail.com>
2025-03-11 09:58:13 -0500

    slightly better

--- make_primorial_stuff.py
+++ make_primorial_stuff.py
@@ -7,6 +7,15 @@ def prod(li):
     for a in li: out *= a
     return out
 
+def filter(li, k):
+    out = []
+    for i in range(len(li)):
+        if i % k == 0: out.append(li[i])
+    return out
+
+def bits(n):
+    return len(bin(int(n))) - 2
+
 while True:
     x = random.randint(1, 750)
     y = random.randint(1, 750)
@@ -14,6 +23,7 @@ while True:
     a, b = min(x, y), max(x, y)
 
     ps = list(primerange(a, b + 1))
+    ps = filter(ps, 1)
 
     if len(ps) < 2: continue
     pr = prod(ps)
@@ -26,15 +36,15 @@ while True:
 
 
     # phi = math.log(rt, 2) * len(ps) / 1.5
-    phi = math.log(pr, 2) * (3 / 5)
+    phi = math.log(pr, 2)**2
 
     al = rt - phi
     be = rt + phi
     
-    print(rt, phi, min(ps), al, max(ps), be)
-    
+    print(rt, phi, min(ps), al, max(ps), be, bits(pr), bits(al), bits(be))
     if min(ps) <= al or max(ps) >= be:
         print("fail")
+        print(ps)
         print(rt, phi, min(ps), al, max(ps), be)
         break
     
